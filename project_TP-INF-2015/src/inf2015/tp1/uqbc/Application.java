bbc28c2f15b23b37a95832d7af918b1288fd7cff
benoit <benoit@benoit-VirtualBox>
2018-02-25 14:42:50 -0500

    commit initial

---
+++ project_TP-INF-2015/src/inf2015/tp1/uqbc/Application.java
@@ -0,0 +1,16 @@
+package inf2015.tp1.uqbc;
+
+/**
+ *
+ * @author benoit
+ */
+public class Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
