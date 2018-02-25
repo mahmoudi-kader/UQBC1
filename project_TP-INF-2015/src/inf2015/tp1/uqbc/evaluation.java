ffae2256d6dc133fc9568d19e8223ffc1d3f18f9
gaddour <mahmoudi.kader@gmail.com>
2018-02-25 15:38:04 -0500

    creation de la classe evaluation

---
+++ project_TP-INF-2015/src/inf2015/tp1/uqbc/evaluation.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inf2015.tp1.uqbc;
+
+/**
+ *
+ * @author Abdelkader
+ */
+public class evaluation {
+    
+}
