07561b906b7752a0b58ff1c645abc6a1fea3556c
benoit <benoit@benoit-VirtualBox>
2018-02-25 15:11:00 -0500

    classe Etudiant

---
+++ project_TP-INF-2015/src/inf2015/tp1/uqbc/Etudiant.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inf2015.tp1.uqbc;
+
+/**
+ * Classe qui représente un étudiant.
+ * @author benoit
+ */
+public class Etudiant {
+    
+    private String codePermanent;
+    private String nom;
+    private String prenom;
+    
+    public String getCodePermanent(){
+        return codePermanent;
+    }
+    
+    void String setCodePermanent(String codePermanent){
+        this.codePermanent = codePermanent;
+    }
+    
+    public String getNom(){
+        return nom;
+    }
+    
+    void String setNom(String nom){
+        this.nom = nom;
+    }
+
+    public String getPrenom(){
+        return codePermanent;
+    }
+    
+    void String setPrenom(String prenom){
+        this.prenom = prenom;
+    }    
+}
