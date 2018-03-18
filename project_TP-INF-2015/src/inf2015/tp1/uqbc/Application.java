c1b898387a9a6a5485f949a3f7ab96e43e89e2f3
benoit <benoit@benoit-VirtualBox>
2018-03-18 15:24:46 -0400

    Merge origin/master
    Conflicts:
    	project_TP-INF-2015/nbproject/project.properties

--- project_TP-INF-2015/src/inf2015/tp1/uqbc/Application.java
+++ project_TP-INF-2015/src/inf2015/tp1/uqbc/Application.java
@@ -1,7 +1,5 @@
 package inf2015.tp1.uqbc;
 
-import inf2015.tp1.uqbc.fichiers.FileReader;
-
 /**
  *
  * @author benoit
@@ -14,12 +12,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO code application logic here
 
-        //String json = new FileReader().StringFromFile("C:\\Users\\DALIL\\Desktop\\ListeEtudiantsCoursBIO1012-Gr10-H18.json");
-        //System.out.println(json);
-        
-        ValidationDonnees repertoire = new ValidationDonnees();
-        repertoire.setNomRepertoire("C:\\Users\\nawal\\Documents\\Cerificat UQAM1\\2018-hiver\\inf2015\\json\\");
-        repertoire.validerRepertoire();
     }
     
 }
