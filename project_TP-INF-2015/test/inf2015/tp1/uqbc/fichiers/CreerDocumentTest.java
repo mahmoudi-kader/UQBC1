03b4627ad3c4027a3d66a01b591022c127f240e6
gaddour <mahmoudi.kader@gmail.com>
2018-03-31 12:09:37 -0400

    validation creation fichiers textes

--- project_TP-INF-2015/test/inf2015/tp1/uqbc/fichiers/CreerDocumentTest.java
+++ project_TP-INF-2015/test/inf2015/tp1/uqbc/fichiers/CreerDocumentTest.java
@@ -6,6 +6,7 @@
 package inf2015.tp1.uqbc.fichiers;
 
 import inf2015.tp1.uqbc.Cours;
+import inf2015.tp1.uqbc.Etudiant;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -61,14 +62,24 @@ public class CreerDocumentTest {
  
         while (it.hasNext()) {
         Boolean result = false;
-        Cours Cours = it.next();
-        String nomCours=Cours.getTitre();
-        File fichier = new File(repertoireDestination+nomCours);
-        Boolean repertoireExiste=fichier.exists();
+        Cours cours = it.next();
+        String nomCours=cours.getTitre();
+        File dossier = new File(repertoireDestination+nomCours);
+        Boolean repertoireExiste=dossier.exists();
         assertTrue(repertoireExiste);
-        
-        }
+        // test si fichiers sont crées dans chacun des repertoires
+       
+    
+            for (Etudiant etudiant : cours.getListeEtudiant()) {
+                    String   nomFichier = cours.getSession() + "_" + cours.getTitre() + "_" + etudiant.getCodePermanent();
+                    File fichierEudiant = new File(dossier.getAbsolutePath()+"\\"+nomFichier+".txt");
+                    Boolean fichierEudiantExiste=fichierEudiant.exists();
+                    assertTrue(fichierEudiantExiste);
+            }
+        //
+
        
     }
     
+    }
 }
