74fd417f026e56f79fe296151518fa30cdea584d
benoit <benoit@benoit-VirtualBox>
2018-04-08 16:03:00 -0400

    Merge origin/master
    Conflicts:
    	project_TP-INF-2015/src/inf2015/tp1/uqbc/Application.java

--- project_TP-INF-2015/src/inf2015/tp1/uqbc/fichiers/JsonWriter.java
+++ project_TP-INF-2015/src/inf2015/tp1/uqbc/fichiers/JsonWriter.java
@@ -1,18 +1,10 @@
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package inf2015.tp1.uqbc.fichiers;
 
 import inf2015.tp1.uqbc.Evaluation;
 
-import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
+import org.json.JSONWriter;
 /**
  *
  * @author DALIL
@@ -23,17 +15,18 @@ public class JsonWriter {
   public static void main(String[] args) {  
 
     Evaluation test = new Evaluation("blabla", "2", "Examen", "Intrea", "40%");  
-
-    ObjectMapper mapper = new ObjectMapper();
     
-    try {  
-
-        // Ecriture dans le fichier JSON    
-        mapper.writeValue(new File("C:\\Users\\DALIL\\Desktop\\Work\\TEST\\exemple.json"), test );
-
-    } catch (IOException e) {  
-        e.printStackTrace();  
-    }  
+    // Ecriture dans le fichier JSON    
+    //mapper.writeValue(new File("C:\\Users\\DALIL\\Desktop\\Work\\TEST\\exemple.json"), test );
+    
+      String szValue = JSONWriter.valueToString(test);
+      try(FileWriter writer = new FileWriter("C:\\\\Users\\\\DALIL\\\\Desktop\\\\Work\\\\TEST\\\\exemple.json")) {
+          writer.write(szValue);
+      } catch (IOException ex) {
+          ex.printStackTrace();
+      }
+      
+    
 
   }  
 }
