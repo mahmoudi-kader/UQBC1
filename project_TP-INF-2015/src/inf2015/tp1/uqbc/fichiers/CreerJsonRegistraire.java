96eefed5db8df8b768a087498774aa64da08e3e9
benoit <benoit@benoit-VirtualBox>
2018-04-12 19:33:08 -0400

    Merge origin/master
    Conflicts:
    	project_TP-INF-2015/src/inf2015/tp1/uqbc/Application.java

--- project_TP-INF-2015/src/inf2015/tp1/uqbc/fichiers/CreerJsonRegistraire.java
+++ project_TP-INF-2015/src/inf2015/tp1/uqbc/fichiers/CreerJsonRegistraire.java
@@ -6,8 +6,6 @@
 package inf2015.tp1.uqbc.fichiers;
 
 import inf2015.tp1.uqbc.Cours;
-import inf2015.tp1.uqbc.Etudiant;
-import inf2015.tp1.uqbc.Evaluation;
 import inf2015.tp1.uqbc.ValidationEvaluations;
 import java.io.File;
 import java.io.FileWriter;
@@ -17,42 +15,12 @@ import java.util.List;
 import org.json.JSONObject;
 
 /**
- *
+ * Cette classe sert a générer le fichier json a être envoyé au registraire.
  * @author Abdelkader
  * 
  */
-
-/*
-En tant que registraire
-Je veux obtenir les notes finales de chaque enseignant
-Afin de générer les relevés de note.
-Critères d’acceptation
-Un fichier par groupe-cours, en format JSON;
-Une note par étudiant; 
-La note doit être sur 100;
-Le fichier ne peut être généré que si toutes les évaluations, totalisant une pondération de 100%, ont été compilées.
-Le fichier peut être regénéré au besoin, si l’enseignant a fait des changements;
-Les étudiants doivent être identifiés par leur code permanent;
-Le cours doit être identifié par son sigle;
-Le groupe-cours doit être identifié par son numéro;
-Le fichier est versionné avec la date et l’heure pour s’y retrouver parmi les multiples versions;
-
-*/
-
- /*
-        //json attendu pour chaque cours
-    {
-	"sigle": "sigle",
-	"groupe": "groupe",
-	"version": "version",
-	"notes": {
-		"matricule1": "note",
-		"matricule2": "note"
-	}
-}
-    */
 public class CreerJsonRegistraire {
-   String repertoireDestination = "C:/Users/nawal/Documents/NetBeansProjects/UQBC1/project_TP-INF-2015/";
+   String repertoireDestination = "./";
    JSONObject obj = new JSONObject();
    JSONObject notes=new JSONObject();
    String cours_groupe="";
