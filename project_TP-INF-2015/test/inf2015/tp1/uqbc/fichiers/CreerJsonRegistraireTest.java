a780ceadbed72abd02a0e4cd3c45f2bea0c552f9
gaddour <mahmoudi.kader@gmail.com>
2018-04-11 23:37:55 -0400

    mise en forme du fichier de sortie JSON

---
+++ project_TP-INF-2015/test/inf2015/tp1/uqbc/fichiers/CreerJsonRegistraireTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inf2015.tp1.uqbc.fichiers;
+
+import inf2015.tp1.uqbc.Cours;
+import inf2015.tp1.uqbc.Etudiant;
+import inf2015.tp1.uqbc.Evaluation;
+import inf2015.tp1.uqbc.ResultatEvaluation;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Abdelkader
+ */
+public class CreerJsonRegistraireTest {
+    
+    public CreerJsonRegistraireTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testCreerJson() throws Exception {
+        System.out.println("CreerJson");
+    }
+    
+}
