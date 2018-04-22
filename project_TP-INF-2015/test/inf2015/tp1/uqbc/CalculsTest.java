d278f6395f9cc48ee9f7b2f9a27dfa3f0fa23609
benoit <benoit@benoit-VirtualBox>
2018-04-22 10:53:09 -0400

    Merge origin/master
    Conflicts:
    	project_TP-INF-2015/src/inf2015/tp1/uqbc/Application.java
    	project_TP-INF-2015/src/inf2015/tp1/uqbc/fichiers/CreerJsonRegistraire.java
    	project_TP-INF-2015/src/inf2015/tp1/uqbc/fichiers/CreerResultatsGroupeCours.java
    	project_TP-INF-2015/test/inf2015/tp1/uqbc/fichiers/CreerJsonRegistraireTest.java

--- project_TP-INF-2015/test/inf2015/tp1/uqbc/CalculsTest.java
+++ project_TP-INF-2015/test/inf2015/tp1/uqbc/CalculsTest.java
@@ -7,8 +7,14 @@ package inf2015.tp1.uqbc;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  *
@@ -16,27 +22,23 @@ import org.junit.Test;
  */
 public class CalculsTest {
 
-    public CalculsTest() {
-    }
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+   
+    private static Cours cours = null;
 
-    @Test
-    public void testCalculs() {
+    @BeforeClass
+    public static void setUpClass() {
         List<Evaluation> listeEvaluation = new ArrayList<Evaluation>();
         List<Etudiant> listeEtudiant = new ArrayList<Etudiant>();
         List<ResultatEvaluation> listeResultatEvaluation = new ArrayList<ResultatEvaluation>();
-        double moyenneCours = 0;
-        double ponderationCours = 0;
-        double moyenneGroupeEvaluation = 0;
-        double notePondereeGlobale = 0;
-        double ponderationGlobaleEtudiant = 0;
-        double notePondereeEvaluation = 0;
         
-        Cours cours1 = new Cours("BIO", "Gr10", "H18", "BlaBla", "1012");
+        cours = new Cours("BIO", "Gr10", "H18", "BlaBla", "1012");
         Etudiant etudiantTemp1 = new Etudiant("SADN27272727", "Saddd", "Naaaa");
         Etudiant etudiantTemp2 = new Etudiant("DASS27272727", "Dasss", "Naaaa");
         listeEtudiant.add(etudiantTemp1);
         listeEtudiant.add(etudiantTemp2);
-        cours1.setListeEtudiant(listeEtudiant);
+        cours.setListeEtudiant(listeEtudiant);
 
         listeResultatEvaluation.add(new ResultatEvaluation(etudiantTemp2, 15.0));
         listeResultatEvaluation.add(new ResultatEvaluation(etudiantTemp1, 13.0));
@@ -54,29 +56,75 @@ public class CalculsTest {
         listeResultatEvaluation.add(new ResultatEvaluation(etudiantTemp1, 36.0));
         listeEvaluation.add(new Evaluation("blabla", "3", "Examen final", "Examen", "40%"));
         listeEvaluation.get(2).setListeResultatEvaluation(listeResultatEvaluation);
-        cours1.setListeEvaluation(listeEvaluation);
-                
-        moyenneCours = Calculs.calculMoyenneGroupeGlobale(cours1.getListeEtudiant(), cours1.getListeEvaluation());
+        cours.setListeEvaluation(listeEvaluation);
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        
+    }
+
+    @Before
+    public void setUp() {
+        
+    }
+
+    @After
+    public void tearDown() {
+        
+    }
+    
+    @Test
+    public void testCalculsMoyenneCours() {
+
+        double moyenneCours = 0;                
+        moyenneCours = Calculs.calculMoyenneGroupeGlobale(cours.getListeEtudiant(), cours.getListeEvaluation());
         System.out.println("moyenneCours : " + moyenneCours);
         Assert.assertEquals(86.5, moyenneCours, 0.001);
         
-        ponderationCours = Calculs.calculPonderationGroupeGlobale(cours1.getListeEtudiant(), cours1.getListeEvaluation());
+    }
+    
+    @Test
+    public void testCalculsPonderationCours() {     
+        double ponderationCours = 0;
+        ponderationCours = Calculs.calculPonderationGroupeGlobale(cours.getListeEtudiant(), cours.getListeEvaluation());
         System.out.println("ponderationCours : " + ponderationCours);
         Assert.assertEquals(100.0, ponderationCours, 0.001);
+    }
+    
+    @Test
+    public void testCalculsMoyenneGroupeEvaluation() {
         
-        moyenneGroupeEvaluation = Calculs.calculMoyenneGroupeEvaluation(cours1.getListeEvaluation().get(0).getListeResultatEvaluation());
+        double moyenneGroupeEvaluation = 0;
+        moyenneGroupeEvaluation = Calculs.calculMoyenneGroupeEvaluation(cours.getListeEvaluation().get(0).getListeResultatEvaluation());
         System.out.println("moyenneGroupeEvaluation : " + moyenneGroupeEvaluation);
         Assert.assertEquals(14.0, moyenneGroupeEvaluation, 0.001);
+    }
+    
+    @Test
+    public void testCalculsNotePonderee() {
         
-        notePondereeEvaluation = Calculs.calculNotePondereeEvaluation(cours1.getListeEtudiant(), cours1.getListeEvaluation(), "SADN27272727", "2");
+        double notePondereeEvaluation  = 0;
+        notePondereeEvaluation = Calculs.calculNotePondereeEvaluation(cours.getListeEtudiant(), cours.getListeEvaluation(), "SADN27272727", "2");
         System.out.println("notePondereeEvaluation : " + notePondereeEvaluation);
         Assert.assertEquals(38.0, notePondereeEvaluation, 0.001);
         
-        notePondereeGlobale = Calculs.calculNotePondereeGlobale(cours1.getListeEtudiant(), cours1.getListeEvaluation(), "SADN27272727");
+    }
+    
+    @Test
+    public void testCalculsNotePondereeGlobale() {
+        
+        double notePondereeGlobale = 0;
+        notePondereeGlobale = Calculs.calculNotePondereeGlobale(cours.getListeEtudiant(), cours.getListeEvaluation(), "SADN27272727");
         System.out.println("notePondereeGlobale : " + notePondereeGlobale);
         Assert.assertEquals(87.0, notePondereeGlobale, 0.001);
-        
-        ponderationGlobaleEtudiant = Calculs.calculPonderationGlobaleEtudiant(cours1.getListeEtudiant(), cours1.getListeEvaluation(), "SADN27272727");
+
+    }
+    
+    @Test
+    public void testCalculsPonderationGlobale() {        
+        double ponderationGlobaleEtudiant = 0;
+        ponderationGlobaleEtudiant = Calculs.calculPonderationGlobaleEtudiant(cours.getListeEtudiant(), cours.getListeEvaluation(), "SADN27272727");
         System.out.println("ponderationGlobaleEtudiant : " + ponderationGlobaleEtudiant);
         Assert.assertEquals(100.0, ponderationGlobaleEtudiant, 0.001);
     }
