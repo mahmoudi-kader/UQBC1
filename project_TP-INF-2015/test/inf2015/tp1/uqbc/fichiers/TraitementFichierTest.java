629e66dfef2c702479fb4d2e3482729db1abf29b
benoit <benoit@benoit-VirtualBox>
2018-04-22 13:00:09 -0400

    refactoring

--- project_TP-INF-2015/test/inf2015/tp1/uqbc/fichiers/TraitementFichierTest.java
+++ project_TP-INF-2015/test/inf2015/tp1/uqbc/fichiers/TraitementFichierTest.java
@@ -1,5 +1,9 @@
 package inf2015.tp1.uqbc.fichiers;
 
+import inf2015.tp1.uqbc.Utilitaires;
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -9,8 +13,28 @@ import org.junit.Test;
 public class TraitementFichierTest {
 
     @Test
-    public void testRamasserAppelerAvecListeNull() {
-
+    public void testRamasser() {
+        
+        List<FichierJSON> liste = new LinkedList();
+        String repertoire = Utilitaires.REPERTOIRE_PAR_DEFAUT+Utilitaires.SEP_FICHIER+"fichiers";
+        
+        TraitementFichiers.ramasser(repertoire, liste);
+        
+        Assert.assertEquals(11, liste.size());
+    }
+    
+        @Test
+    public void testRamasserException() {
+        
+        List<FichierJSON> liste = new LinkedList();
+        String repertoire = Utilitaires.REPERTOIRE_PAR_DEFAUT+Utilitaires.SEP_FICHIER+"FGHFHFGH";
+        
+        try{
+            TraitementFichiers.ramasser(repertoire, liste);
+            Assert.fail("On ne devrait pas se rendre ici, car la méthode doit lancer une exception.");
+        }catch(Throwable t){
+            Assert.assertTrue(t.getMessage().contains("n'est pas accessible"));
+        }
     }
 
 }
